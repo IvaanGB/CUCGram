53f42582d0fabb6ca3a3a096b1046ae9b16053f0
IvaanGB <igonzale4@cuc.edu.co>
2018-06-04 16:41:54 -0500

    Interfaz Login y Create creadas, sin conexión Firebase, CardViews

---
+++ app/src/main/java/com/ivaangb/cucgram/PrincipalLogin.java
@@ -0,0 +1,24 @@
+package com.ivaangb.cucgram;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.ivaangb.cucgram.view.CreateAccountActivity;
+
+public class PrincipalLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal_login);
+    }
+
+    public void goCreateAccount(View view){
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
+    }
+
+
+}
---
+++ app/src/main/java/com/ivaangb/cucgram/view/CreateAccountActivity.java
@@ -0,0 +1,29 @@
+package com.ivaangb.cucgram.view;
+
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+
+import com.ivaangb.cucgram.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        showToolbar(getResources().getString(R.string.toolbarTittleCreateAccount), true);
+    }
+
+
+    public void showToolbar(String tittle, boolean upButton){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(tittle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }
+
+}
